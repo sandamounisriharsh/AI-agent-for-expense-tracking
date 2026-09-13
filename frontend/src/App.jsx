47af223651cd25cad31b4d0677535063505e9a08
sandamounisriharsh <sriharsh040@gmail.com>
2026-09-13 13:04:38 +0530

    Fix login and signup import casing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 import Home from "./pages/home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 
 function App() {
